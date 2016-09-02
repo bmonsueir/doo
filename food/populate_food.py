439c636ab9c70aaa6f8290586e4aa198832603a6
Bill Monsueir <bmonsueir@gmail.com>
2016-09-02 19:24:02 +0000

    reading txt and csv data

---
+++ food/populate_food.py
@@ -0,0 +1,13 @@
+#run once to populate the database with nutritional information
+import numpy as np
+from pandas import Series,DataFrame
+import pandas as pd
+
+foods = pd.read_csv('food/static/food/FOOD_DES.txt',sep=',',  header=None,  usecols = [0,2,4])
+dbcol = pd.read_csv('food/static/food/NUTR_DEF.txt', sep = ' ', header=None,  usecols = [1,2,4])
+nutrition = pd.read_csv('food/static/food/NUT_DAT.csv', sep = ',', header=None, usecols = [0,1,2])
+for i in range(len(nutrition)):
+    
+    print(nutrition[1][i])
+
+
--- food/static/food/populate_food.py
+++
@@ -1,8 +0,0 @@
-#run once to populate the database with nutritional information
-import numpy as np
-from pandas import Series,DataFrame
-import pandas as pd
-
-dframe = pd.read_csv('FOOD_DES.txt',sep=',', usecols = [0,2,4])
-
-print(dframe)
