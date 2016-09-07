c0d9e3d6e4975b1eb048c23196645119d32b12b9
Bill Monsueir <bmonsueir@gmail.com>
2016-09-07 16:49:00 +0000

    added get value for nutrition

--- food/populate_food.py
+++ food/populate_food.py
@@ -20,58 +20,12 @@ def load_nut(cols):
     nutrition = pd.read_csv('food/static/food/NUT_DAT.csv', sep = ',', names = cols)
     return nutrition
 
+def get_values(food, test):
+    nutrition = load_nut(['food','test','value'])
+    content = nutrition.loc[nutrition['food'] == food]
+    content = content.loc[content['test'] == test]
+    return content['value']
 
-def find_food_names(partial_name):
-    """
-    input partial word that is being looked up and return food names that match
-    """
-    content = []
-    if len(partial_name) > 2:
-        foods = load_foods([2])
-        print(partial_name)
-        for index, food in foods.iterrows():
-            if partial_name.lower() in food[2].lower() and food[2] not in content:
-                content.append(food[2])
-            
-    return content
-    
-def find_food_code(food_name):
-    """
-    input food name and return food code
-    """
-    content = []
-    if len(food_name) > 2:
-        foods = load_foods([2])
-        print(food_name)
-        for index, food in foods.iterrows():
-            if food_name.lower() in food[2].lower() and food[2] not in content:
-                content.append(food[2])
-            
-    return content
-
-
-def find_components(food_code):
-    """
-    input is the code number and return nutritional data
-    """
-    content = {}
-    
-    return content
-    
-    
-def find_food_name(food_name):
-    """
-    input food names and return food name
-    """
-    content = []
-    if len(food_name) > 2:
-        foods = load_foods([2])
-        print(food_name)
-        for index, food in foods.iterrows():
-            if food_name.lower() in food[2].lower() and food[2] not in content:
-                content.append(food[2])
-            
-    return content
     
 def dict_food_codes():
     food_codes = load_foods(['code','group','name','desc0','desc1','desc2','desc3','desc4','desc5','desc6','desc7','desc8','desc9'])
@@ -83,14 +37,14 @@ def dict_food_codes():
             if type(food_code[desc_index]) is str:
                 desc += food_code[desc_index].lower() + " "
         content[food_code['code']] =  food_code['name'].lower() + ' (' + desc + ')'
-    print(content)
+    return content
 
 def dict_test_codes():
     test_codes = load_defs(['code','unit','abrev','name'])
     content = {}
     for index, test_code in test_codes.iterrows():
         content[test_code['code']] =  test_code['name'].lower() + ' (' + test_code['unit'] + ')'
-    print(content)   
+    return content
 
-dict_food_codes()
-
+# dict_food_codes()
+print(get_values(1001, 405))
