d8e484f19f45a6573eb69ac89493a729d5c462ad
Bill Monsueir <bmonsueir@gmail.com>
2016-10-01 00:18:24 +0000

    blog continues

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField( max_length=255, )
+    date = models.DateTimeField('date created', default=datetime.now)
+    user = models.ForeignKey(User)
+   
+    def __str__(self):
+        return self.title 
--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,10 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+from django.views.generic import ListView, DetailView
+from models import Post
 
 urlpatterns = [
-    url(r'^$', views.home, name ='home'),
+    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html")),
 
 ]
